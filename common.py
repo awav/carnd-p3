9e6e7b51b7874041312ce5cb1a383c333ba5666a
Artem Artemev <art.art.v@gmail.com>
2017-02-26 23:48:02 +0000

    First commit

---
+++ common.py
@@ -0,0 +1,174 @@
+# The MIT License (MIT)
+# 
+# Copyright 2017 Artem Artemev, im@artemav.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+# and associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2 as cv
+import os
+import re
+
+def load_image(filename, color='RGB'):
+    im = cv.imread(filename, cv.IMREAD_COLOR)
+    return cvt_color(im, color=color, src='BGR')
+
+def cvt_color(im, color='RGB', src='BGR'):
+    if src == 'BGR':
+        colors = {'RGB':cv.COLOR_BGR2RGB,
+                  'HLS':cv.COLOR_BGR2HLS,
+                  'HSV':cv.COLOR_BGR2HSV,
+                  'LUV':cv.COLOR_BGR2LUV,
+                  'YUV':cv.COLOR_BGR2YUV,
+                  'YCrCb':cv.COLOR_BGR2YCrCb}
+    elif src == 'RGB':
+        colors = {'BGR':cv.COLOR_RGB2BGR,
+                  'HLS':cv.COLOR_RGB2HLS,
+                  'HSV':cv.COLOR_RGB2HSV,
+                  'LUV':cv.COLOR_RGB2LUV,
+                  'YUV':cv.COLOR_RGB2YUV,
+                  'YCrCb':cv.COLOR_RGB2YCrCb}
+    else:
+        raise ValueError("Source image can be either in RGB or BGR colormap")
+    if color not in colors:
+        raise ValueError("{0} color is not allowed".format(color))
+    return cv.cvtColor(im, colors[color])
+
+def list_images(folder, regex=None):
+    return np.array([filename for filename in _list_images(folder)])
+
+def _list_images(folder, regex=None):
+    """
+    Generator to list images in directory. Default regex parameter is None and
+    it will search for png and jpg images.
+    """
+    if not os.path.isdir(folder):
+        ## TOOD: log this message
+        return
+    folder = os.path.normpath(folder)
+    folder_len = len(folder)
+    default_pattern = '.*\.(png|jpg|jpeg|PNG|JPG|JPEG)$'
+    regex = default_pattern if regex == None else regex
+    regex_compiled = re.compile(regex)
+    for subdir, mid, files in os.walk(folder):
+        subdir = os.path.normpath(subdir)
+        if subdir[:folder_len] != folder:
+            continue
+        if  files == []:
+            continue
+        for filename in files:
+            if re.match(regex_compiled, filename) is None:
+                continue
+            yield os.path.join(subdir, filename)
+
+def load_images(*dirs, color='RGB'):
+    return np.array([im for im in _load_images(*dirs, color=color)])
+    
+def _load_images(*dirs, color='RGB'):
+    """
+    Generator to list images in specified dirs.
+    By default it loads images in RGB.
+    """
+    subdirs = set()
+    for folder in dirs:
+        for filename in _list_images(folder):
+            subdirname = os.path.dirname(filename)
+            if subdirname not in subdirs:
+                subdirs.add(subdirname)
+                print(subdirname)
+            yield load_image(filename, color=color)
+
+def serialize(obj, filename):
+    with open(filename, 'wb') as fd:
+        pickle.dump(obj, fd)
+
+def show_image(ims, ncols=1, nrows=1, window_title=None, titles=None, cmaps=None):
+    """
+    Show images as tiles in grid.
+    """
+    fig, axes = plt.subplots(ncols=ncols, nrows=nrows, squeeze=True)
+    if window_title is not None:
+        fig.canvas.set_window_title(window_title)
+    size = len(ims)
+    if ncols == 1 and nrows == 1:
+        if cmaps is not None:
+            axes.imshow(ims, cmap=cmaps)
+        else:
+            axes.imshow(ims)
+        if titles is not None:
+            axes.set_title(titles)
+        axes.get_xaxis().set_visible(False)
+        axes.get_yaxis().set_visible(False)
+    elif ncols == 1 or nrows == 1:
+        n = max(ncols, nrows)
+        for i in range(n):
+            ax = axes[i]
+            if cmaps is not None:
+                ax.imshow(ims[i], cmap=cmaps[i])
+            else:
+                ax.imshow(ims[i])
+            if titles is not None:
+                ax.set_title(titles[i])
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+    else:
+        for r in range(nrows):
+            for c in range(ncols):
+                ax = axes[r][c]
+                i = r * ncols + c
+                if i >= size:
+                    ax.axis('off')
+                    continue
+                if cmaps is not None:
+                    ax.imshow(ims[i], cmap=cmaps[i])
+                else:
+                    ax.imshow(ims[i])
+                if titles is not None:
+                    ax.set_title(titles[i])
+                ax.get_xaxis().set_visible(False)
+                ax.get_yaxis().set_visible(False)
+    fig.tight_layout()
+    fig.subplots_adjust(wspace=0)
+    fig.show()
+
+def equalize_hist(im, show=False):
+    im_eq = cv.cvtColor(im, cv.COLOR_RGB2YCrCb)
+    zeros = np.zeros(im_eq.shape[:2])
+    im_eq[:,:,0] = cv.equalizeHist(im_eq[:,:,0], zeros)
+    im_eq = cv.cvtColor(im_eq, cv.COLOR_YCrCb2RGB)
+    if show == True:
+        show_images(im, im_eq, 'original', 'equalized', 'Histogram equalization')
+    return im_eq
+
+## In [5]: data = model.VehiclesDataset(); m = model.CarModel(); data = m.prepare(data); m.fit(data)
+## data/vehicles/GTI_MiddleClose
+## data/vehicles/GTI_Left
+## data/vehicles/KITTI_extracted
+## data/vehicles/GTI_Far
+## data/vehicles/GTI_Right
+## data/OwnCollection/vehicles/Right
+## data/OwnCollection/vehicles/Left
+## data/OwnCollection/vehicles/Far
+## data/OwnCollection/vehicles/MiddleClose
+## data/non-vehicles/GTI
+## data/non-vehicles/Extras
+## data/OwnCollection/non-vehicles/Right
+## data/OwnCollection/non-vehicles/Left
+## data/OwnCollection/non-vehicles/Far
+## data/OwnCollection/non-vehicles/MiddleClose
+## Test accuracy: 0.99821
+## AUC score: 0.99997
+
---
+++ dataproc.py
@@ -0,0 +1,95 @@
+# The MIT License (MIT)
+#
+# Copyright 2017 Artem Artemev, im@artemav.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+# and associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import cv2 as cv
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy.random as rnd
+
+import common
+import pickle
+import time
+import os
+
+import importlib
+
+class Augment():
+    def __new__(cls, *args, **kvargs):
+        raise ValueError('Not possible to create instance of this class')
+    @staticmethod
+    def setseed(seed=None):
+        if seed == None:
+           a = rnd.uniform(0,1)
+           b = np.uint32(rnd.uniform(1, 1000))
+           rnd.seed(np.uint32(time.time() * a) + b)
+        else:
+           rnd.seed(seed)
+    @staticmethod
+    def brightness(im, lbl, low=0.3, high=1.2, p=0.5, np=0.5):
+        if rnd.choice([1,0], p=[p, np]) == 1:
+            hsv = common.cvt_color(img, color='HSV', src='RGB')
+            adjust = rnd.uniform(low=low, high=high)
+            hsv[:,:,2] *= adjust
+            return (cv2.cvtColor(hsv.astype(np.uint8), cv2.COLOR_HSV2RGB), lbl)
+        return im, lbl
+    @staticmethod
+    def flip_horizontal(im, lbl, p=0.5, np=0.5):
+        assert((p + np) == 1)
+        if rnd.choice([1,0], p=[p, np]) == 1:
+            return (np.fliplr(im), -lbl)
+        return (im, lbl)
+    @staticmethod
+    def shadow(im, lbl, low=0.4, hight=0.9, p=0.5, np=0.5):
+        if rnd.choice([1,0], p=[p, np]) == 1:
+            height, width, _ = im.shape
+            adjust = rnd.uniform(low=0, high=width)
+            xu, xd = rnd.uniform(low=0, high=width, size=2)
+            alpha = height / (xu - xd)
+            beta = - (k * xu)
+            side = rnd.uniform([0, 1])
+            for row in height:
+                col = np.uint32((col - beta) / alpha)
+                if side == 0:
+                    im[row,:col] *= adjust
+                else:
+                    im[row,col:] *= adjust
+            return im.astype(np.uint8), lbl
+        return im, lbl
+    @staticmethod
+    def crop_height(im, lbl, top=(0.3, 0.450), bottom=(0.075, 0.175), p=1, np=0):
+        yt = np.uint32(rnd.uniform(*top))
+        yb = np.uint32(rnd.uniform(*bottom))
+        return im[yt:-yb], lbl
+
+class TrackDataset():
+    def __init__(self, data_path='data/', driving_log='driving_log.csv', img_path='IMG/', show=True):
+        imgs_path = os.path.join(data_path, img_path)
+        csv_path = os.path.join(data_path, driving_log)
+        dt = pd.read_csv(csv_path)
+        center = pd.DataFrame({'image':dt.center, 'steering':dt.steering})
+        right = pd.DataFrame({'image':dt.right, 'steering':dt.steering})
+        left = pd.DataFrame({'image':dt.left, 'steering':dt.steering})
+        self._data = pd.concat([center, left, right], ignore_index=True)
+    def load(self, filename='data/dataset.p'):
+        with open(filename, 'rb') as fd:
+             self.__dict__ = pickle.load(fd)
+    def save(self, filename='data/dataset.p'):
+        with open(filename, 'wb') as fd:
+             pickle.dump(self.__dict__, fd)
