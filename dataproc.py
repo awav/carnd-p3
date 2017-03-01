babd982abdf7840ba5ac5c59b6f720f08429a8a4
Artem Artemev <art.art.v@gmail.com>
2017-03-01 00:40:46 +0000

    Continue

--- dataproc.py
+++ dataproc.py
@@ -20,6 +20,7 @@ import cv2 as cv
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy.random as rnd
 
@@ -29,6 +30,8 @@ import time
 import os
 
 import importlib
+
+from sklearn.model_selection import train_test_split
 
 class Augment():
     def __new__(cls, *args, **kvargs):
@@ -79,22 +82,72 @@ class Augment():
         return im[yt:-yb], lbl
 
 class TrackDataset():
-    def __init__(self, data_path='data/', driving_log='driving_log.csv', img_path='IMG/', show=True):
-        imgs_path = os.path.join(data_path, img_path)
+    def __init__(self, data_path='data/', driving_log='driving_log.csv', img_path='IMG/'):
+        self._images_path = os.path.join(data_path, img_path)
         csv_path = os.path.join(data_path, driving_log)
+        self._load_steering_data(csv_path)
+        self._split_valid_train()
+        self._skewed_angles = None
+        self._batch_x_shape = None
+        self._batch_x_shape = None 
+        self._batch_y_shape = None 
+        self._mode = None
+        self._color = None
+    def init_batch_generator(self, batch_size=128, image_size=(128,128), mode='train', color='RGB'):
+        self._batch_x_shape = (batch_size, *image_size)
+        self._batch_x_shape = (batch_size, *image_size)
+        self._batch_y_shape = (batch_size, 1)
+        if mode != 'train' and mode != 'valid':
+            raise ValueError('Unknown mode {0}'.format(mode))
+        self._mode = mode
+        self._color = color
+    def batch_generator():
+        batch_size = self._batch_x_shape[0]
+        x = np.zeros(self._batch_x_shape)
+        y = np.zeros(self._batch_y_shape)
+        while self:
+            #for i in range(batch_size):
+            yield x, y
+    def show_histograms(self):
+        matplotlib.rc('xtick', labelsize=15)
+        matplotlib.rc('ytick', labelsize=15)
+        fig, (ax1, ax2) = plt.subplots(nrows=2)
+        fig.canvas.set_window_title('Initial dataset')
+        ax1.set_title('normal scale')
+        ax2.set_title('log scaled')
+        ax2.set_yscale('log')
+        self._dt.steering.hist(ax=ax1, alpha=0.5, bins=100)
+        self._dt.steering.hist(ax=ax2, alpha=0.5, bins=100)
+        fig.show()
+        fig, (ax1, ax2) = plt.subplots(nrows=2)
+        fig.canvas.set_window_title('Augmented by steering angles dataset')
+        ax1.set_title('normal scale')
+        ax2.set_title('log scaled')
+        ax2.set_yscale('log')
+        self._data.steering.hist(ax=ax1, alpha=0.5, bins=100)
+        self._data.steering.hist(ax=ax2, alpha=0.5, bins=100)
+        fig.show()
+    def _load_steering_data(self, csv_path):
         dt = pd.read_csv(csv_path)
         right_steering = self._adjust_angles(dt.steering, side='right')
         left_steering = self._adjust_angles(dt.steering, side='left')
-        center = pd.DataFrame({'image':dt.center, 'steering':df.steering})
+        center = pd.DataFrame({'image':dt.center, 'steering':dt.steering})
         right = pd.DataFrame({'image':dt.right, 'steering':right_steering})
         left = pd.DataFrame({'image':dt.left, 'steering':left_steering})
+        self._dt = dt
         self._data = pd.concat([center, left, right], ignore_index=True)
-    def _adjust_angles(angles, side='right', adjust=0.25):
+    def _split_valid_train(self, skewmax=1000):
+        vals = self._data.steering.value_counts()
+        self._skewed_angles = vals.index[vals.values >= skewmax]
+        #nonskewed_data = 
+        #self._xnorm, self._xval, self._ynorm, self._yval = train_test_split(test=0.15)
+    def _adjust_angles(self, angles, side='right', adjust=0.25):
         if side == 'right':
             adjust *= -1
-        elif:
+        elif side != 'left':
             raise ValueError('Unknown adjustment side')
-        return np.arctan(np.tan(angles) + adjust)
+        vals = np.arctan(np.tan(angles) + adjust)
+        return vals.clip(lower=-1.0, upper=1.0)
     def load(self, filename='data/dataset.p'):
         with open(filename, 'rb') as fd:
              self.__dict__ = pickle.load(fd)
